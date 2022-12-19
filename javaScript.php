f6759b16371d245bfffd9bb2d6880df7f0bf8523
Jiří Hollan <jiri.hollan@gmail.com>
2022-12-19 17:07:59 +0100

    v javaScript.php dodana funkcija

--- javaScript.php
+++ javaScript.php
@@ -1,6 +1,11 @@
 <?php
 echo'
 <script>
+function fajlFunctionPhp() {
+document.getElementById("p4a").innerHTML = "<b>4a. php funkcija</b>";
+}
+
+
 document.getElementById("p4").innerHTML = "<b>4. skript v PHP</b>";
 </script>
 ';
--- poskus.php
+++ poskus.php
@@ -19,12 +19,14 @@ require_once('javaScript.php');
 <p id="p2a">to je p2a---2a.skript v head</p>
 <p id="p3">to je p3---3.link v head funkcija</p>
 <p id="p4">to je p4---4.skript v PHP</p>
+<p id="p4a">to je p4a---4a.funkcija v PHP</p>
 <p id="p5">to je p5---5.link v head skript</p>
 <p id="p6">to je p6---6.link v body funkcija</p>
 <p id="p7">to je p7---7.link v body skript</p>
 <button type="button" onclick="headFunction()">Skript v head</button>
 <button type="button" onclick="fajlFunction()">link v head</button>
 <button type="button" onclick="fajlFunctionBody()">link v body</button>
+<button type="button" onclick="fajlFunctionPhp()">funkcija v php</button>
 <script>
 document.getElementById("p1").innerHTML = "<b>1. skript v body</b>";
 </script>
